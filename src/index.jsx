70ea6d872ee470fa5a6ebd05803e57ec8f358c22
Ryan Vannin <ryan@plastical.com>
2017-01-04 16:25:27 +0100

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,76 @@
+/**
+ * External dependencies
+ */
+import { Component, PropTypes } from 'react';
+import shallowEqual from 'shallowequal';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import debugFactory from 'debug';
+
+/**
+ * Internal dependencies
+ */
+import { isRequestingPagesForQuery, isRequestingPage } from './selectors';
+import { requestPages, requestPage } from './state';
+
+const debug = debugFactory( 'query:page' );
+
+class QueryPages extends Component {
+	componentWillMount() {
+		this.request(this.props);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (this.props.pageSlug === nextProps.pageSlug &&
+				shallowEqual( this.props.query, nextProps.query)) {
+			return;
+		}
+
+		this.request(nextProps);
+	}
+
+	request(props) {
+		const single = !!props.pageSlug;
+
+		if (!single && !props.requestingPages) {
+			debug(`Request page list using query ${props.query}`);
+			props.requestPages(props.query);
+		}
+
+		if (single && !props.requestingPage) {
+			debug(`Request single page ${props.pageSlug}`);
+			props.requestPage(props.pageSlug);
+		}
+	}
+
+	render() {
+		return null;
+	}
+}
+
+QueryPages.propTypes = {
+	pageSlug: PropTypes.string,
+	query: PropTypes.object,
+	requestingPages: PropTypes.bool,
+	requestPages: PropTypes.func
+};
+
+QueryPages.defaultProps = {
+	requestPages: () => {}
+};
+
+export default connect(
+	(state, ownProps) => {
+		const {pageSlug, query} = ownProps;
+		return {
+			requestingPage: isRequestingPage(state, pageSlug),
+			requestingPages: isRequestingPagesForQuery(state, query)
+		};
+	},
+	(dispatch) => {
+		return bindActionCreators({
+			requestPages,
+			requestPage
+		}, dispatch);
+	}
+)(QueryPages);
---
+++ src/utils.js
@@ -0,0 +1,36 @@
+/**
+ * External dependencies
+ */
+import omitBy from 'lodash/omitBy';
+
+const DEFAULT_PAGES_QUERY = {
+  _embed: true,
+  number: 10,
+  offset: 0,
+  order_by: 'menu',
+  order: 'ASC',
+  fields: 'all_with_meta'
+};
+
+/**
+ * Returns a normalized pages query, excluding any values which match the
+ * default page query.
+ *
+ * @param  {Object} query Pages query
+ * @return {Object}       Normalized pages query
+ */
+export function getNormalizedPagesQuery(query) {
+	return omitBy(query, (value, key) => DEFAULT_PAGES_QUERY[key] === value);
+}
+
+/**
+ * Returns a serialized pages query, used as the key in the
+ * `state.pages.queries` state object.
+ *
+ * @param  {Object} query  Pages query
+ * @return {String}        Serialized pages query
+ */
+export function getSerializedPagesQuery(query = {}) {
+	const normalizedQuery = getNormalizedPagesQuery(query);
+	return JSON.stringify(normalizedQuery).toLocaleLowerCase();
+}
